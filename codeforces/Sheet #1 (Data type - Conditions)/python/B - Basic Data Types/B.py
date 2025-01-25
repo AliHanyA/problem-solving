2f5d5e6434459a0946cf0531ed5d3f81fc84abcf
Ali Hany <164428722+AliHanyA@users.noreply.github.com>
2025-01-25 14:14:45 -0800

    Create B.py

---
+++ codeforces/Sheet #1 (Data type - Conditions)/python/B - Basic Data Types/B.py
@@ -0,0 +1,7 @@
+input_values = input().split()
+ 
+int_value = int(input_values[0])
+long_long_value = int(input_values[1])
+char_value = input_values[2]
+float_value = float(input_values[3])
+double_value = float(input_values[4])
