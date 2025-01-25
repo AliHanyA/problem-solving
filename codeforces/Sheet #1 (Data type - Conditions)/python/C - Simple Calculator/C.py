64d48d97653313e1d2915a4a02ac40b885f0e09e
Ali Hany <164428722+AliHanyA@users.noreply.github.com>
2025-01-25 14:15:28 -0800

    Create C.py

---
+++ codeforces/Sheet #1 (Data type - Conditions)/python/C - Simple Calculator/C.py
@@ -0,0 +1,5 @@
+input_values = input().split()
+ 
+num1 = int(input_values[0])
+num2 = int(input_values[1])
+print(f'{num1} + {num2} = {num1+num2}\n{num1} * {num2} = {num1*num2}\n{num1} - {num2} = {num1-num2}')
