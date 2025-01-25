bd0bc8f6b1fb22842d8a5a587193b1e230536fe3
Ali Hany <164428722+AliHanyA@users.noreply.github.com>
2025-01-25 15:10:41 -0800

    Delete leetcode directory

--- leetcode/2114. Maximum Number of Words Found in Sentences/MaximumNumberOf WordsFoundInSentences.py
+++
@@ -1,6 +0,0 @@
-class Solution(object):
-    def countOdds(self, low, high):
-        x = (high + 1) // 2
-        y = low // 2
-        return x - y
-        
--- leetcode/2114. Maximum Number of Words Found in Sentences/MaximumNumberWordsFoundInSentences.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def mostWordsFound(self, sentences):
-        tot = 0
-        for x in sentences:
-            words = x.split()
-            tot = max(tot, len(words))
-
-        return tot
-            
