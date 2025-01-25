bbc309cc03df94b82a74bb24b3aaf91eacd78d2d
Ali Hany <164428722+AliHanyA@users.noreply.github.com>
2025-01-25 14:05:34 -0800

    C.cpp

---
+++ codeforces/Sheet #1 (Data type - Conditions)/C++/B - Basic Data Types/C.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int intValue;
+    long long longLongValue;
+    char charValue;
+    float floatValue;
+    double doubleValue;
+    cin >> intValue >> longLongValue >> charValue >> floatValue >> doubleValue;
+    cout<<intValue<<endl
+    <<longLongValue<<endl
+    <<charValue<<endl<<
+    floatValue<<endl<<
+    doubleValue;
+ 
+}
