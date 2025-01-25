55ab91b0da4c4f56c7529e84131bab25b77e3379
Ali Hany <164428722+AliHanyA@users.noreply.github.com>
2025-01-25 14:11:50 -0800

    Create I.cpp

---
+++ codeforces/Sheet #1 (Data type - Conditions)/C++/I - Welcome for you with Conditions/I.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+ 
+int main() {
+    int A,B;
+    cin>>A>>B;
+    if (A<B)
+        cout<<"No";
+    else
+        cout<<"Yes";
+ 
+    return 0;
+}
